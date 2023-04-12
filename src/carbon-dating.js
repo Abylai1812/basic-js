96a0157e537afb54bf2197c56b671411568eb423
Abylai <abylaikhhann@mail.ru>
2023-04-12 20:04:46 +0500

    init:task carbon,count

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,10 +17,26 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+function dateSample(sampleActivity) {
+
+  if (typeof sampleActivity !== 'string') {
+    return false;
+  }
+  
+  const activity = parseFloat(sampleActivity);
+  
+
+  if (isNaN(activity) || activity <= 0 || activity > MODERN_ACTIVITY) {
+    return false;
+  }
+  
+
+  const age = Math.ceil(-1 * HALF_LIFE_PERIOD * Math.log(activity / MODERN_ACTIVITY));
+  
+  return age;
 }
+
 
 module.exports = {
   dateSample
